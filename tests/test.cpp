2d76fcad9ffdcf88f6cd6b66bb196cbffda43ae3
Myung Kyu Song <singsong@paran.com>
2023-07-26 12:59:57 +0900

    message

--- tests/test.cpp
+++ tests/test.cpp
@@ -660,6 +660,7 @@ void fblr_test()
     std::string fname = "../wall_column.dat";
     read_file(fname,lines); // -> read_file(fname, wall, cols);
 
+
     {
         MkPoint pnt(50,50,0);
         double theta = -43; // counter clockwise
@@ -676,6 +677,7 @@ void fblr_test()
 }
 
 int main_()
+
 {
     // TODO: to incorporate GLFW, GLAD for the testing
     // int argc;
@@ -691,36 +693,36 @@ int main_()
 
     // fblr_test();
 
-    //     for (int i=0;i<4;i++)   {
-    //         cols[i].SetHeight(20);
-    //         cols[i].SetWidth(20);
-    //     }
+        for (int i=0;i<4;i++)   {
+            cols[i].SetHeight(20);
+            cols[i].SetWidth(20);
+        }
 
-    //     MkPoints ori;
-    //     MkPoints trace;
-    //     MkDouble res;
-    //     MkLines lines;
-    //     std::string fname = "../wall_column.dat";
-    //     read_file(fname,lines); // -> read_file(fname, wall, cols);
+        MkPoints ori;
+        MkPoints trace;
+        MkDouble res;
+        MkLines lines;
+        std::string fname = "../wall_column.dat";
+        read_file(fname,lines); // -> read_file(fname, wall, cols);
 
 
-    //     {
-    //         MkPoint pnt(50,50,0);
-    //         double theta = -43; // counter clockwise
-    //         for (int i=0;i<lines.GetSize();i++) {lines[i].SetFiniteness(true);}
-    //         std::cout << "lines size: " << lines.GetSize() << "\n";
+        {
+            MkPoint pnt(50,50,0);
+            double theta = -43; // counter clockwise
+            for (int i=0;i<lines.GetSize();i++) {lines[i].SetFiniteness(true);}
+            std::cout << "lines size: " << lines.GetSize() << "\n";
 
-    //         // res = get_fblr(theta, pnt, wall, cols);
-    //         // res = get_fblr(theta, pnt, lines); // -> get_fblr(theta, pnt, wall, cols);
-    //         res = get_fblr(theta, pnt, wall, cols);
+            // res = get_fblr(theta, pnt, wall, cols);
+            // res = get_fblr(theta, pnt, lines); // -> get_fblr(theta, pnt, wall, cols);
+            res = get_fblr(theta, pnt, wall, cols);
 
-    //         std::cout << "f: " << res[0] << ", b:" << res[1] << ", l:" << res[2] << ", r:" << res[3] << "\n";
-    //         scan_test(lines,theta,res);
-    //     }
-    // }
+            std::cout << "f: " << res[0] << ", b:" << res[1] << ", l:" << res[2] << ", r:" << res[3] << "\n";
+            scan_test(lines,theta,res);
+        }
+    }
 
-    // // pnts_tst();
+    // pnts_tst();
     
-    // return 0;
+    return 0;
 }
 
